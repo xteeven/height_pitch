30a1424a67b9c36e3be765bbd2f19e4b517190fc
Christoph Parstorfer <c.parstorfer@k5factory.com>
2021-04-23 14:15:34 +0200

    Add distinct noise sounds at certain frequencies Fix initial volume level calculation

--- HeightCodingFrequencyTest/Assets/VolumeFading/VolumeCurve.cs
+++ HeightCodingFrequencyTest/Assets/VolumeFading/VolumeCurve.cs
@@ -8,7 +8,9 @@ namespace VolumeFading
         const float MT_EVEREST = 8848f;
 
         [SerializeField]
-        internal AnimationCurve altitudeToVolumeCurve = new AnimationCurve(new Keyframe(0f, 1f), new Keyframe(1f, 0f));
+        internal AnimationCurve altitudeToVolumeCurve = new AnimationCurve(
+                new Keyframe(0f, 0f), new Keyframe(0.5f, 1f), new Keyframe(1f, 0f)
+            );
 
         [SerializeField, Range(0f, MT_EVEREST)]
         internal float minAltitude = 0f;
@@ -20,39 +22,63 @@ namespace VolumeFading
         internal bool useLocalPosition = false;
 
         private float range = 0f;
-        private float lastSeenY = 0f;
+        private float lastSeenY = -1f;
         private AudioSource audioSource;
-        private float relativeVolume = 1f;
+        [SerializeField, HideInInspector]
+        private float volumeFactor = 1f;
 
         private void OnValidate()
         {
             if (maxAltitude <= minAltitude)
                 maxAltitude = minAltitude + 10f;
-            relativeVolume = GetComponent<AudioSource>().volume;
+            volumeFactor = GetComponent<AudioSource>().volume;
         }
 
         private void OnEnable()
         {
             range = maxAltitude - minAltitude;
             audioSource = GetComponent<AudioSource>();
+
+            if (range > 0f)
+            {
+                lastSeenY = useLocalPosition ? transform.localPosition.y : transform.position.y;
+                UpdateForAltitudeChange(updateOutsideOfRange:true);
+            }
+            else
+            {
+                // no range, no volume change!
+                enabled = false;
+                UpdateVolume(0f);
+            }
         }
 
         private void Update()
         {
             float y = useLocalPosition ? transform.localPosition.y : transform.position.y;
-            if (y != lastSeenY && range > 0)
+            if (y != lastSeenY)
             {
                 lastSeenY = y;
-                float percent = (y - minAltitude) / range;
-                if (percent > 1f || percent < 0f)
-                    return;
-                else
-                {
-                    float val = altitudeToVolumeCurve.Evaluate(percent);
-                    val = Mathf.Min(1f, Mathf.Max(0f, val));
-                    audioSource.volume = val * relativeVolume;
-                }
+                UpdateForAltitudeChange();
             }
+        }
+
+        private void UpdateForAltitudeChange(bool updateOutsideOfRange = false)
+        {
+            float percent = (lastSeenY - minAltitude) / range;
+            if (percent > 1f || percent < 0f)
+            {
+                if (!updateOutsideOfRange)
+                    return;
+            }
+            percent = Mathf.Min(1f, Mathf.Max(0f, percent));
+            UpdateVolume(percent);
+        }
+
+        private void UpdateVolume(float percent)
+        {
+            float val = altitudeToVolumeCurve.Evaluate(percent);
+            val = Mathf.Min(1f, Mathf.Max(0f, val));
+            audioSource.volume = val * volumeFactor;
         }
     }
 }
